2bd438fe99e51e5e9ed2781007acd858133c40f7
brenhamp <bpozzi@trinity.edu>
2022-03-27 23:27:58 -0500

    added some styling to the page, fixed project images

--- brenham-p-portfolio/src/components/Contact/index.js
+++ brenham-p-portfolio/src/components/Contact/index.js
@@ -40,15 +40,15 @@ function Contact() {
     <section>
       <h1 data-testid="h1tag">Contact me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
-        <div>
+        <div class="contact-form">
           <label htmlFor="name">Name:</label>
           <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
         </div>
-        <div>
+        <div class="contact-form">
           <label htmlFor="email">Email address:</label>
           <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
         </div>
-        <div>
+        <div class="contact-form">
           <label htmlFor="message">Message:</label>
           <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
         </div>
--- brenham-p-portfolio/src/components/Footer/index.js
+++ brenham-p-portfolio/src/components/Footer/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Footer() {   
     return (
-    <footer className="footer">
-        <a href="https://github.com/brenhamp"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="GitHub" className="icon"/></a>
+    <footer className="footer float-right align-right fixed-bottom">
+        <a href="https://github.com/brenhamp"><img src="https://img.icons8.com/material/48/github.png" alt="GitHub" className="icon"/></a>
         <a href="https://www.linkedin.com/in/brenham-tyler-pozzi/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
     </footer>
     )
